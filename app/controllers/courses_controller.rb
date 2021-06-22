2a0cd4e00ff7db7da4449273d6955592c0aa8a81
dianabasilio <dianabasilio99@gmail.com>
2021-06-22 14:07:16 -0500

    new

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,21 @@
 class CoursesController < ApplicationController
 
 	def index
+		@courses= Course.all
 
 	end
 
+	def lists
+		@courses= Course.all
+
+	end
+
+
 	def new
+
+	end
+
+	def infoC1
 
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'courses#index'
-  get 'courses/new', to: 'courses#new'
+  get 'new', to: 'courses#new'
   get 'about', to: 'pages#about'
+  get 'lists', to: 'courses#lists'
+  get 'infoC1', to: 'courses#infoC1'
+  resources :students, except: [:destroy]
 
 end
